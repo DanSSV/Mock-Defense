6afc4ad704c779be3aa2f83886e2dae603dca8bb
Dan <123217422+DanSSV@users.noreply.github.com>
2023-09-08 17:09:14 +0800

    Add files via upload

---
+++ driver/user_find.php
@@ -0,0 +1,35 @@
+<?php
+// Step 1: Include the database connection file
+include('../db/dbconn.php');
+
+// Step 2: Query the history_1 table to find user_id
+$plateNumber = 'GHI-123';
+$status = 'active';
+
+$query = "SELECT user_id FROM history_1 WHERE plate_number = ? AND status = ?";
+$stmt = $conn->prepare($query);
+$stmt->bind_param("ss", $plateNumber, $status);
+$stmt->execute();
+$stmt->bind_result($userId);
+$stmt->fetch();
+$stmt->close();
+
+// Step 3: Query the users table to find the passenger's name
+if (!empty($userId)) {
+    $query = "SELECT name FROM users WHERE user_id = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $userId);
+    $stmt->execute();
+    $stmt->bind_result($name);
+    $stmt->fetch();
+    $stmt->close();
+
+    // Step 4: Display the passenger's name in the <h5> tag
+    echo '<h5 style="color: white;">Passenger: ' . $name . '</h5>';
+} else {
+    echo '<h5 style="color: white;">Passenger not found</h5>';
+}
+
+// Step 5: Close the database connection
+$conn->close();
+?>
